b8abb96d66bac8aaf0acb802b6ae07a94fb7ec65
Aya Boukhris <eyaboukh@gmail.com>
2025-04-07 00:26:57 +0100

    user service

---
+++ bookyBack/User/src/main/java/com/example/user/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.example.user.controllers;
+
+import com.example.user.models.User;
+import com.example.user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+} 
