2aebf200c7eae7a9a4343e9f34814328e945caa4
nousseiba kaabi <nousseiba.kaabi@esprit.tn>
2025-04-20 01:53:13 +0100

    serveur de configuration

---
+++ bookyBack/Gateway/src/main/java/com/esprit/microservice/gateway/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.esprit.microservice.gateway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/carts/**").permitAll()
+                        .pathMatchers("/payment/**").permitAll()
+                        .pathMatchers("/**").permitAll()
+                        .anyExchange().authenticated()
+                );
+        return http.build();
+    }
+}
