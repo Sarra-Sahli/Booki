1028bd1d333cd712cc0f157808f32a5db0177aec
Sarra-Sahli <sarah1sahlii1@gmail.com>
2025-04-20 17:02:50 +0200

    Update environment.ts

--- bookyFront/src/environments/environment.ts
+++ bookyFront/src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
     apiUrl: 'http://localhost:8083',
     cartUrl: 'http://localhost:8082/carts',
     gatewayUrl: 'http://localhost:8083',
-    complaintUrl: 'http://localhost:8083/api/complaints',
+    complaintUrl: 'http://localhost:3000/api/complaints',
     googleMapsKey: 'dev-key-123'
   };
